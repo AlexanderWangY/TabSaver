5433b7feb8e30748a3f90755e11299e68f82b303
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2023-02-03 01:31:47 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
+let inputBtn = document.getElementById('input-btn');
+
+inputBtn.addEventListener("click", saveLead() {
+    console.log('click')
+});
