57f63e304b09c5da9a66cb334ec702ce921d3d14
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2023-02-03 01:43:38 -0600

    updates n stuff

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let myLeads = [];
+/*let myLeads = [];
 const inputEl = document.getElementById('input-el');
 let inputBtn = document.getElementById('input-btn');
 
-inputBtn.addEventListener("click", saveLead() {
-    console.log('click')
-});
+inputBtn.addEventListener('click', (e) => {
+    myLeads.push("www.google.com");
+
+});*/
+
+const add = (a, b) => a + b + 100;
+const a = 10;
+const b = 20;
+
+
+console.log(add(a, b));
