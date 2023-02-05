7ac07d3a4e4fcd834350ae47566d86a986ae967f
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2023-02-05 01:29:39 -0600

    beta version

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ inputBtn.addEventListener('click', function() {
 function renderLeads() {
     let listItems = "";
 for (let i = 0; i < myLeads.length; i++) {
-    //listItems += "<li> <a target='_blank' href=' https://" + myLeads[i] + " '>" + myLeads[i] + "</a></li>";
     listItems += `
     <li>
         <a target='_blank' href='${myLeads[i]}'>
